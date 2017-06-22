039c8a08be9f0f69d7f637a9f524aac419569706
Wurmatron <wurmatron@gmail.com>
2017-06-22 15:20:37 -0400

    Added Item Storage to Habitat

--- src/main/java/wurmatron/spritesofthegalaxy/common/reference/NBT.java
+++ src/main/java/wurmatron/spritesofthegalaxy/common/reference/NBT.java
@@ -8,4 +8,7 @@ public class NBT {
 	public static final String MAX_POPULATION = "maxPopulation";
 	public static final String FOOD = "food";
 	public static final String LASTUPDATE = "lastUpdate";
+	public static final String INVENTORY = "inv";
+	public static final String AMOUNT = "amount";
+	public static final String ITEM = "item";
 }
--- src/main/java/wurmatron/spritesofthegalaxy/common/tileentity/TileHabitatCore.java
+++ src/main/java/wurmatron/spritesofthegalaxy/common/tileentity/TileHabitatCore.java
@@ -1,15 +1,18 @@
 package wurmatron.spritesofthegalaxy.common.tileentity;
 
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.NetworkManager;
 import net.minecraft.network.play.server.SPacketUpdateTileEntity;
 import net.minecraft.util.ITickable;
 import wurmatron.spritesofthegalaxy.common.config.Settings;
 import wurmatron.spritesofthegalaxy.common.reference.NBT;
-import wurmatron.spritesofthegalaxy.common.utils.LogHandler;
 import wurmatron.spritesofthegalaxy.common.utils.MutiBlockHelper;
+import wurmatron.spritesofthegalaxy.common.utils.StackHelper;
 
-import javax.annotation.Nullable;
+import java.util.HashMap;
 
 public class TileHabitatCore extends TileMutiBlock implements ITickable {
 
@@ -19,6 +22,7 @@ public class TileHabitatCore extends TileMutiBlock implements ITickable {
 	private int maxPopulation = Settings.maxPopulation;
 	private int food = Settings.populationFoodRequirement * Settings.maxPopulation;
 	private long lastUpdate;
+	private HashMap <String, Integer> items = new HashMap <> ();
 	// TEMP
 	private double population = 1;
 
@@ -33,11 +37,9 @@ public class TileHabitatCore extends TileMutiBlock implements ITickable {
 		}
 		// TODO Add Support for over 1 update
 		if (lastUpdate + UPDATE_TIME <= System.currentTimeMillis ()) {
-			LogHandler.serverInfo ("Population: " + getPopulation () + " / " + getMaxPopulation ());
-			LogHandler.serverInfo ("Food: " + getFood ());
 			handleUpdate ();
 			lastUpdate = System.currentTimeMillis ();
-			world.markAndNotifyBlock (pos,world.getChunkFromBlockCoords (pos),world.getBlockState (pos), world.getBlockState (pos),3);
+			world.markAndNotifyBlock (pos,world.getChunkFromBlockCoords (pos),world.getBlockState (pos),world.getBlockState (pos),3);
 		}
 	}
 
@@ -48,6 +50,14 @@ public class TileHabitatCore extends TileMutiBlock implements ITickable {
 		maxPopulation = nbt.getInteger (NBT.MAX_POPULATION);
 		food = nbt.getInteger (NBT.FOOD);
 		lastUpdate = nbt.getLong (NBT.LASTUPDATE);
+		NBTTagCompound inv = nbt.getCompoundTag (NBT.INVENTORY);
+		for (int index = 0; index < inv.getSize (); index++) {
+			NBTTagCompound temp = inv.getCompoundTag (Integer.toString (index));
+			ItemStack item = new ItemStack (temp.getCompoundTag (NBT.ITEM));
+			int amount = temp.getInteger (NBT.AMOUNT);
+			if (item != ItemStack.EMPTY && item.getItem () != Item.getItemFromBlock (Blocks.AIR))
+				items.put (convertToData (item),amount);
+		}
 		// Temp
 		population = nbt.getDouble ("population");
 		markDirty ();
@@ -59,6 +69,16 @@ public class TileHabitatCore extends TileMutiBlock implements ITickable {
 		nbt.setInteger (NBT.MAX_POPULATION,maxPopulation);
 		nbt.setInteger (NBT.FOOD,food);
 		nbt.setLong (NBT.LASTUPDATE,lastUpdate);
+		NBTTagCompound invList = new NBTTagCompound ();
+		int index = 0;
+		for (String item : items.keySet ()) {
+			index++;
+			NBTTagCompound temp = new NBTTagCompound ();
+			temp.setInteger (NBT.AMOUNT,items.get (item));
+			temp.setTag (NBT.ITEM,convertToStack (item).writeToNBT (new NBTTagCompound ()));
+			invList.setTag (Integer.toString (index),temp);
+		}
+		nbt.setTag (NBT.INVENTORY,invList);
 		// Temp
 		nbt.setDouble ("population",population);
 		super.writeToNBT (nbt);
@@ -96,20 +116,45 @@ public class TileHabitatCore extends TileMutiBlock implements ITickable {
 			setPopulation (getPopulation () - Math.abs (getFood ()));
 	}
 
+	public void setStack (ItemStack stack,int amount) {
+		if (stack != ItemStack.EMPTY && stack.getItem () != Item.getItemFromBlock (Blocks.AIR))
+			items.put (convertToData (stack),amount);
+	}
+
+	public void addStack (ItemStack stack) {
+		if (items.containsKey (convertToData (stack))) {
+			stack.setCount (1);
+			setStack (stack,items.get (convertToData (stack)) + stack.getCount ());
+		} else {
+			stack.setCount (1);
+			setStack (stack,stack.getCount ());
+		}
+	}
+
+	private String convertToData (ItemStack item) {
+		item.setCount (1);
+		return StackHelper.convertToString (item);
+	}
+
+	private ItemStack convertToStack (String stack) {
+		return StackHelper.convertToStack (stack);
+	}
+
+
 	public void handleUpdate () {
 		growPopulation ();
 	}
 
 	@Override
-	public SPacketUpdateTileEntity getUpdatePacket() {
-		NBTTagCompound tag = new NBTTagCompound();
+	public SPacketUpdateTileEntity getUpdatePacket () {
+		NBTTagCompound tag = new NBTTagCompound ();
 		writeToNBT (tag);
-		return new SPacketUpdateTileEntity(pos, 0, tag);
+		return new SPacketUpdateTileEntity (pos,0,tag);
 	}
 
 	@Override
-	public void onDataPacket(NetworkManager net, SPacketUpdateTileEntity packet) {
+	public void onDataPacket (NetworkManager net,SPacketUpdateTileEntity packet) {
 		population = packet.getNbtCompound ().getDouble ("population");
-		readFromNBT (packet.getNbtCompound());
+		readFromNBT (packet.getNbtCompound ());
 	}
 }
